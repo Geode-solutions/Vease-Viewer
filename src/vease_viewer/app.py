0c27d96292004fb59af6b33946c565aa72e5d2ff
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-01-14 15:52:06 +0100

    custom vease protocols

--- src/vease_viewer/app.py
+++ src/vease_viewer/app.py
@@ -6,9 +6,8 @@ from opengeodeweb_viewer.vtkw_server import _Server, run_server
 # Local application imports
 from vease_viewer.rpc.protocols import VtkVeaseViewerView
 
-_Server.registerVtkWebProtocol(VtkVeaseViewerView())
-
 def run_viewer():
+    _Server.custom_protocols.append(VtkVeaseViewerView())
     run_server()
 
 if __name__ == "__main__":
