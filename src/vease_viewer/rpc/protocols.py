134bac7bae89d594d7b52e30d185906fd5fe98ea
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-09-29 13:35:14 +0200

    ignore decorator

--- src/vease_viewer/rpc/protocols.py
+++ src/vease_viewer/rpc/protocols.py
@@ -28,7 +28,7 @@ class VtkVeaseViewerView(vtk_protocols.vtkWebProtocol):
 
         return {"microservice_version": metadata.distribution("vease_viewer").version}
 
-    @exportRpc("kill")
+    @exportRpc("kill")  # type: ignore
     def kill(self) -> None:
         print("Manual viewer kill, shutting down...", flush=True)
         os._exit(0)
