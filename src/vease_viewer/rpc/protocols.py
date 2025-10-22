8ae40d39ddf8801577c966089412f5f84bfe5a03
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-10-22 21:07:46 +0200

    test

--- src/vease_viewer/rpc/protocols.py
+++ src/vease_viewer/rpc/protocols.py
@@ -11,7 +11,7 @@ from wslink import register as exportRpc
 # Local application imports
 
 
-class VtkVeaseViewerView(vtk_protocols.vtkWebProtocol):  # type: ignore
+class VtkVeaseViewerView(vtk_protocols.vtkWebProtocol):
     prefix = "vease_viewer."
     schemas_dict = get_schemas_dict(os.path.join(os.path.dirname(__file__), "schemas"))
 
